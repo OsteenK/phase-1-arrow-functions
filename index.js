763f8967951dac63d20357be908d8467edfedeed
OsteenK <osteen.mutinda@student.moringa.com>
2022-11-24 02:32:10 +0300

    Completed assingment

---
+++ index.js
@@ -0,0 +1,18 @@
+const add = (a, b) => {
+    console.log(`Adding ${a}`);
+    console.log(`Adding ${b}`);
+    return a + b;
+}
+const square = (a) => {
+    console.log(`multiplying ${a}`);
+    console.log(`multiplying ${a}`);
+    return a * a;
+}
+const divide = (a, b) =>  {
+  console.log(`divide ${a}`); 
+  console.log(`divide ${b}`);
+  return 2000 / 100;
+}
+
+
+
